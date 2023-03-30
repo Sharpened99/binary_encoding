500cc803b5a90f3a9acf3b8d3705b11f752f104d
Alexander Reinecke <a.reinecke@tu-bs.de>
2023-03-30 23:12:46 +0200

    Rest of filewriting and correcting some mistakes

--- helpers.py
+++ helpers.py
@@ -43,7 +43,57 @@ def rotate_right(x: int, max_bits: int, n = 1) -> int:
     return x
 
 
+def get_overlap(x: int, y: int) -> int:
+    count = 0
+    while x > 0 and y > 0:
+        count += (x & 1) & (y & 1)
+        x >>= 1
+        y >>= 1
+
+    return count
+
+
 def zero_pad_to_len(s: str, length: int) -> str:
     while len(s) < length:
         s = "0" + s
     return s
+
+
+def get_max_dec_len(bits) -> int:
+    return len(str(2 ** bits))
+
+
+def get_n_token_numbers(rotation_tokens: int, bits: int) -> list:
+    min_num = 2 ** rotation_tokens - 1
+    n_token_numbers = [*range(min_num, 2 ** bits)]
+    i = 0
+    while i < len(n_token_numbers):
+        if count_tokens(n_token_numbers[i]) != rotation_tokens:
+            n_token_numbers.pop(i)
+        else:
+            i += 1
+    return n_token_numbers
+
+
+def get_starter_line(bits) -> str:
+    s = ""
+    for i in range(get_max_dec_len(bits) - 2):
+        s += " "
+    s += "no;"
+    for i in range(get_max_dec_len(bits) - 2):
+        s += " "
+    s += "dec;"
+    for i in range(bits - 2):
+        s += " "
+    s += "bin;\n"
+    return s
+
+
+def format_code_line(num: int, bits, code_index) -> str:
+    max_dec_len = get_max_dec_len(bits)
+
+    padded_code_no = zero_pad_to_len(str(code_index), max_dec_len)
+    padded_dec_num = zero_pad_to_len(str(num), max_dec_len)
+    padded_bin_num = zero_pad_to_len(str(bin(num))[2:], bits)
+
+    return padded_code_no + "; " + padded_dec_num + "; " + padded_bin_num + ";\n"
--- main.py
+++ main.py
@@ -11,22 +11,22 @@ global code_index
 
 
 def get_inputs():
-    global BITS
-    global NUMBER_CONTAINERS
-    global CONTAINER_SIZE
-    global FILTER_SIZE
-    global THEORETICAL_LIMIT
+    global BITS, NUMBER_CONTAINERS, CONTAINER_SIZE
 
     # Get inputs
     BITS = int(input("Input the number of bits: "))
     NUMBER_CONTAINERS = int(input("Input the number of containers per \"filter\": "))
     CONTAINER_SIZE = int(input("Input the size of the container (5/9/27/54): "))
-    FILTER_SIZE = NUMBER_CONTAINERS * CONTAINER_SIZE
-    THEORETICAL_LIMIT = 2 ** BITS - 1
     print()
 
 
-def maximum_all_perm_tokens() -> int:
+def generate_constants():
+    global FILTER_SIZE, THEORETICAL_LIMIT
+    FILTER_SIZE = NUMBER_CONTAINERS * CONTAINER_SIZE
+    THEORETICAL_LIMIT = 2 ** BITS - 1
+
+
+def maximum_all_perm_tokens() -> tuple[int, int]:
     usable_codes = 0
     items_filter = 0
     tokens = 1
@@ -36,20 +36,18 @@ def maximum_all_perm_tokens() -> int:
         items_filter += n_choose_k(BITS - 1, tokens - 1)
         tokens += 1
 
-    return tokens - 1
+    return tokens - 1, items_filter
 
 
-def find_rotations(tokens: int) -> int:
+def get_number_rotations(tokens: int) -> tuple[int, int]:
     rotations = 0
-    items_filter = 0
-    for i in range(1, tokens):
-        items_filter += n_choose_k(BITS - 1, i - 1)
+    items_filter = maximum_all_perm_tokens()[1]
 
     while items_filter + tokens <= NUMBER_CONTAINERS * CONTAINER_SIZE:
         items_filter += tokens
         rotations += 1
 
-    return rotations
+    return rotations, items_filter
 
 
 def get_number_leftover_codes(leftover_space: int, rotation_tokens: int, usable_codes: int) -> int:
@@ -71,17 +69,23 @@ def print_stats(items_filter, usable_codes):
 
 
 def find_usable_codes():
-    get_inputs()
+    if len(args) == 5:
+        global BITS, NUMBER_CONTAINERS, CONTAINER_SIZE
+        BITS = int(args[2])
+        NUMBER_CONTAINERS = int(args[3])
+        CONTAINER_SIZE = int(args[4])
+    else:
+        BITS = 0
+        get_inputs()
+
+    generate_constants()
 
     usable_codes = 0
-    items_filter = 0
     ###########################################################################################
     # Find max tokens where every permutation is a valid code
-    max_tokens = maximum_all_perm_tokens()
-
+    max_tokens, items_filter = maximum_all_perm_tokens()
     for i in range(1, max_tokens + 1):
         usable_codes += n_choose_k(BITS, i)
-        items_filter += n_choose_k(BITS - 1, i - 1)
 
     print("---Token filling---")
     print("Max tokens where every perm is valid code:", max_tokens)
@@ -91,10 +95,9 @@ def find_usable_codes():
     rotation_tokens = max_tokens + 1
 
     if max_tokens < BITS and items_filter + rotation_tokens <= FILTER_SIZE:
-        rotations = find_rotations(rotation_tokens)
+        rotations, items_filter = get_number_rotations(rotation_tokens)
 
         usable_codes += rotations * BITS
-        items_filter += rotations * rotation_tokens
 
         print("---Rotations---")
         print("There are ", rotations, "rotations with", rotation_tokens, "tokens that are usable")
@@ -107,18 +110,20 @@ def find_usable_codes():
 
     if leftover_codes > 0:
         print("---Leftover space filling---")
-        print("There are ", leftover_space, "spaces left in the chests to fill with individual codes\n"
-                                            "that have", rotation_tokens, "tokens")
+        print("There are ", leftover_space, "spaces left in the chests to fill with individual codes"
+                                            " that have", rotation_tokens, "tokens")
         print("There are", leftover_codes, "individual codes (I think)")
     ###########################################################################################
     # Results
     print_stats(items_filter, usable_codes)
 
+    return usable_codes
+
 
 def generate_permutations_list(max_tokens: int):
     numbers = [*range(1, 2 ** BITS)]
 
-    i = 0
+    i = 2 ** max_tokens - 1
     while i < len(numbers):
         if count_tokens(numbers[i]) > max_tokens:
             numbers.pop(i)
@@ -132,90 +137,164 @@ def generate_permutations_list(max_tokens: int):
     return numbers
 
 
-def format_code_line(num: int) -> str:
-    max_dec_len = get_max_dec_len()
-    padded_bin_num = zero_pad_to_len(str(bin(num))[2:], BITS)
-    padded_dec_num = zero_pad_to_len(str(num), max_dec_len)
-    global code_index
-    padded_file_line = zero_pad_to_len(str(code_index), max_dec_len)
-    return padded_file_line + "; " + padded_dec_num + "; " + padded_bin_num + ";\n"
-
-
-def get_max_dec_len():
-    return len(str(2 ** BITS))
-
-
 def write_all_perm_codes() -> str:
     s = ""
-    max_all_perm_tokens: int = maximum_all_perm_tokens()
+    max_all_perm_tokens: int = maximum_all_perm_tokens()[0]
     max_token_numbers = generate_permutations_list(max_all_perm_tokens)
-    for num in max_token_numbers:
-        s += format_code_line(num)
+    for i in range(len(max_token_numbers)):
+        num = max_token_numbers[i]
+        if i > 0 and count_tokens(num) > count_tokens(max_token_numbers[i - 1]):
+            s += get_separator_line()
         global code_index
+        s += format_code_line(num, BITS, code_index)
         code_index += 1
     return s
 
 
-def write_all_rotation_codes() -> str:
-    rotation_tokens = maximum_all_perm_tokens() + 1
+def write_all_rotation_codes() -> tuple[str, list[int]]:
+    rotation_tokens = maximum_all_perm_tokens()[0] + 1
     s = ""
-    min_num = 2 ** rotation_tokens - 1
-    number_of_rotations = find_rotations(rotation_tokens)
-    for i in range(BITS):
-        num = rotate_left(min_num, BITS, i)
-        s += format_code_line(num)
+    number_of_rotations = get_number_rotations(rotation_tokens)[0]
+
+    n_token_numbers = get_n_token_numbers(rotation_tokens, BITS)
+
+    # BE or LE sorting, comment out the following line if you want LE sorting
+    n_token_numbers.sort(reverse = True)
+
+    no_rotational_dup = remove_rot_dup(n_token_numbers)
+
+    ########################################
+    used_codes = []
+
+    for i in range(number_of_rotations):
+        val = no_rotational_dup[i]
+        for rot in range(BITS):
+            rotated = rotate_right(val, BITS, rot)
+            global code_index
+            s += format_code_line(rotated, BITS, code_index)
+            code_index += 1
+        if i < number_of_rotations - 1:
+            s += get_separator_line()
+        used_codes.append(val)
+
+    return s, used_codes
+
+
+def remove_rot_dup(n_token_numbers):
+    no_rotational_dup = []
+    for new_val in n_token_numbers:
+        counter = 0
+        for rot in range(BITS):
+            rotated = rotate_right(new_val, BITS, rot)
+            if no_rotational_dup.__contains__(rotated):
+                counter += 1
+        if counter == 0:
+            no_rotational_dup.append(new_val)
+    return no_rotational_dup
+
+
+def sort_to_overlap(to_sort):
+    new_list = []
+    new_list.insert(0, to_sort.pop(0))
+
+    while len(to_sort) > 0:
+        collective_overlap = []
+        for i in range(len(to_sort)):
+            collective_overlap.insert(i, 0)
+            for to_compare in new_list:
+                collective_overlap[i] += get_overlap(to_sort[i], to_compare)
+
+        lowest_overlap_index = 0
+        for i in range(len(collective_overlap)):
+            if collective_overlap[i] < collective_overlap[lowest_overlap_index]:
+                lowest_overlap_index = i
+
+        new_list.append(to_sort.pop(lowest_overlap_index))
+
+    return new_list
+
+
+def write_individual_codes(used_codes) -> str:
+    max_all_perm_tokens = maximum_all_perm_tokens()[0]
+    rotation_tokens = max_all_perm_tokens + 1
+    number_of_rotations, items_filter = get_number_rotations(rotation_tokens)
+    leftover_space = FILTER_SIZE - items_filter
+
+    usable_codes = 0
+    for i in range(1, max_all_perm_tokens):
+        usable_codes += n_choose_k(BITS, i)
+    usable_codes += number_of_rotations * BITS
+
+    leftover_codes = get_number_leftover_codes(leftover_space, rotation_tokens, usable_codes)
+    s = ""
+
+    n_token_numbers_wo_rot = get_n_token_numbers(rotation_tokens, BITS)
+    for n in used_codes:
+        n_token_numbers_wo_rot.remove(n)
+
+    free_codes = remove_rot_dup(n_token_numbers_wo_rot)
+
+    # BE or LE sorting: Comment out the following line if you want LE sorting
+    free_codes.sort(reverse = True)
+    ###########################################
+    free_codes = sort_to_overlap(free_codes)
+
+    bin_nums = []
+    for n in free_codes:
+        bin_nums.append(bin(n))
+
+    print(bin_nums)
+
+    individual_codes = free_codes[0: leftover_codes]
+    for num in individual_codes:
         global code_index
+        s += format_code_line(num, BITS, code_index)
         code_index += 1
-    # TODO
+
     return s
 
 
-def get_separator_line():
-    length = get_max_dec_len() * 2 + BITS + 5
+def get_separator_line() -> str:
+    length = get_max_dec_len(BITS) * 2 + BITS + 5
     s = ""
     for i in range(length):
         s += "-"
     return s + "\n"
 
 
-def write_codes_to_file():
+def write_codes_to_file(number_usable_codes):
     file = open("usable_codes.csv", "w")
     global code_index
     code_index = 1
-    content: str = ""
+    content: str = get_starter_line(BITS)
     ###########################################################################################
+    codes = []
 
     content += write_all_perm_codes()
-    content += get_separator_line()
-    content += write_all_rotation_codes()
-    # TODO
+    if code_index < number_usable_codes:
+        content += get_separator_line()
+        rots, codes = write_all_rotation_codes()
+        content += rots
+    if code_index < number_usable_codes:
+        content += get_separator_line()
+        content += write_individual_codes(codes)
 
     ###########################################################################################
     file.write(content)
     file.close()
 
 
-def do_math():
-    while True:
-        nums = input("Give n and k: ")
-        nums_array = nums.split(" ")
-        n = int(nums_array[0])
-        k = int(nums_array[1])
-        print(n, ";", k, ":", n_choose_k(n, k), ";", n_choose_k(n - 1, k - 1))
-
-
 if __name__ == '__main__':
     args = sys.argv
+    print(args)
 
-    if len(args) < 2 or len(args) > 3:
+    if len(args) < 2 or len(args) > 5:
         print("Incorrect number of arguments")
         exit(0)
 
     if args[1].startswith("-c"):
-        find_usable_codes()
+        usable = find_usable_codes()
         if args[1].__contains__("w"):
-            write_codes_to_file()
-    elif args[1] == "-m":
-        do_math()
+            write_codes_to_file(usable)
     elif args[1] == "-i":
         pass
