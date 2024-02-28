41af344ae67296eebad969fa8d442049ea1eab2d
Alexander Reinecke <a.reinecke@tu-bs.de>
2024-02-28 11:29:47 +0100

    test overlap function

--- test.py
+++ test.py
@@ -7,6 +7,10 @@ import main
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(count_tokens(127), 7)  # add assertion here
+
+        self.assertEqual(get_overlap(7, 8), 0)
+        self.assertEqual(get_overlap(127, 7), 3)
+
         main.BITS = 7
         main.FILTER_SIZE = 54
         self.assertEqual(main.maximum_all_perm_tokens(), 4)
