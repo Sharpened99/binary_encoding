70bf26cbb9d94409d506ec3136b0719b5814efb7
Alexander R. <71272215+Sharpened99@users.noreply.github.com>
2024-08-29 17:00:50 +0200

    Fix typo

--- helpers.py
+++ helpers.py
@@ -7,7 +7,7 @@ def fact(n: int) -> int:
 
 def n_choose_k(n: int, k: int) -> int:
     if n < k:
-        raise AttributeError("n must be smaller or equal to k;", n, k)
+        raise AttributeError("n must be greater or equal to k;", n, k)
     return int(
         fact(n) /
         (fact(k) * fact(n - k)
